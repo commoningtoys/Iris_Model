11e9e6cab5d24c1caf57e096ae033029a9cc8ac3
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-07-24 10:50:56 +0200

    update agent and const

--- js/agent.js
+++ js/agent.js
@@ -246,7 +246,7 @@ class Agent {
             this.working = true;
             this.workingTimer = 2 * TIME_SCALE;
             // this.restingTime -= task.value;
-            this.restingTime /= 2;
+            this.restingTime /= 2;// here add slider that chenges how much resting time is decreased
             // this.makeInfo(`AGENT: ${this.ID} is resting. Resting time ${this.restingTime}`);
             this.FLD = MAXIMUM;// ?? should the FLD go to maximum??
             // this.updateAttributes(task, true);
