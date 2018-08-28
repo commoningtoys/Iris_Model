772dee934d9f3039f8da29ed0846df48058c44ad
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-08-28 16:38:01 +0200

    change the agents to be all geniesser

--- js/model.js
+++ js/model.js
@@ -7,7 +7,7 @@ class IrisModel {
         for (let i = 0; i < num_agents; i++) {
             // let randomIndex = Math.floor(Math.random() * 4);
             let index = i % AGENT_BEHAVIORS.length;
-            this.agents.push(new Agent(TASK_LIST, i + 1, false, AGENT_BEHAVIORS[3]));
+            this.agents.push(new Agent(TASK_LIST, i + 1, false, AGENT_BEHAVIORS[2]));
         }
         // add players
         for (let i = 0; i < num_players || 0; i++) {
