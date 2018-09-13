5bce85baa5b24dcdd00034e69ecacd6083fbd402
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-09-13 11:55:46 +0200

    update agent.js and utils.js by moving linear regression function

--- js/agent.js
+++ js/agent.js
@@ -875,50 +875,22 @@ class Agent {
 
         }
         if (this.behavior === 'geniesser' || this.behavior === 'perfectionist') {
-            const MEDIUM_SKILL = MAXIMUM / 2;
-            let taskSkill = []
-            let x_s = [];
-            for (let i = 0; i < this.preferenceArchive.length; i++)x_s.push(i);
+            /**
+             * both geniesser and perfectionist have their preference for the
+             * skill with higher value. but the preference offsets from the
+             * skill value depending on how slope of the skill over time.
+             */
+            let x_s = [];// x_s they represent the time units that
+            for (let i = 0; i < this.preferenceArchive.length; i++)x_s.push(i);// here we fill it 
 
             for (const task of tasks) {
-
+                // here we fill the y_s with all the values of the skill
                 let y_s = this.preferenceArchive.map(result => result.preferences[task].skill_level);
                 let pref = this.preferences[task].skill_level;
                 let offset = 5 + (fldOffset * 3);
-                pref += linearRegression(y_s, x_s).slope > 0 ? offset : -offset;
-                // this.preferences[task].task_preference += linearRegression(y_s, x_s).slope > 0 ? -10 : 10;
+                pref += linearRegression(y_s, x_s).slope > 0 ? offset : -offset; // here we compute the slope and we look if it is positive or negative
                 this.preferences[task].task_preference = pref;
                 this.preferences[task].task_preference = clamp(this.preferences[task].task_preference, MINIMUM, MAXIMUM);
-            }
-            // console.log(linearRegression(y_s, x_s));
-            // for (const task of taskSkill) {
-            //     this.preferences[task.task_name].task_preference += task.task_skill_offset;
-            //     this.preferences[task.task_name].task_preference = clamp(this.preferences[task.task_name].task_preference, MINIMUM, MAXIMUM);
-            // }
-
-            function linearRegression(y, x) {
-                let lr = {};
-                let n = y.length;
-                let sum_x = 0;
-                let sum_y = 0;
-                let sum_xy = 0;
-                let sum_xx = 0;
-                let sum_yy = 0;
-
-                for (let i = 0; i < n; i++) {
-
-                    sum_x += x[i];
-                    sum_y += y[i];
-                    sum_xy += (x[i] * y[i]);
-                    sum_xx += (x[i] * x[i]);
-                    sum_yy += (y[i] * y[i]);
-                }
-
-                lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
-                lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
-                lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
-
-                return lr;
             }
         }
 
--- js/utils.js
+++ js/utils.js
@@ -70,3 +70,33 @@ function saveRAWData(agents) {
 function modelTime() {
     return TS_FRACTION * TIME_SCALE;
 }
+/**
+ * compute the linear regression of a line 
+ * @param {Array} y positions on the y axis
+ * @param {Array} x positions on the x axis
+ * @returns Object containing slope, intercept and r2
+ */
+function linearRegression(y, x) {
+    let lr = {};
+    let n = y.length;
+    let sum_x = 0;
+    let sum_y = 0;
+    let sum_xy = 0;
+    let sum_xx = 0;
+    let sum_yy = 0;
+
+    for (let i = 0; i < n; i++) {
+
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += (x[i] * y[i]);
+        sum_xx += (x[i] * x[i]);
+        sum_yy += (y[i] * y[i]);
+    }
+
+    lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+    lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
+    lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
+
+    return lr;
+}
