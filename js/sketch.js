4f5bf888d0ef49064ab6d47c15de22eb5d180600
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-09-17 14:28:48 +0200

    add form interactionsw

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,4 @@
-const PLAYER_ID = 'PLAYER'
+const PLAYER_ID = 'PLAYER';
 
 let singleView = true;
 function WIDTH() {
@@ -28,7 +28,7 @@ function draw() {
   for (let i = 0; i < loops; i++) {
     irisModel.update();
   }
-  if(frameCount % 15 == 0)irisModel.show();
+  if (frameCount % 15 == 0) irisModel.show();
   document.getElementById('whatFrameRate').innerHTML = 'Frame rate: <br>' + frameRate();
   // noLoop();
 }
@@ -62,26 +62,75 @@ function drawInfos(agent) {
  * here we add all the event listeners 
  */
 let recordData = document.getElementById('record-data')
-recordData.addEventListener('click', () =>{
+recordData.addEventListener('click', () => {
   irisModel.recordData();
 });
 
-$('#show-menu').click(()=>{
+$('#show-menu').click(() => {
   $('.menu').toggle('fast');
 })
 
-$('#close-menu').click(()=>{
+$('#close-menu').click(() => {
   $('.menu').toggle('fast');
 })
 
-$('#select-behavior').click(el =>{
+$('#select-behavior').click(el => {
   // console.log($("#select-behavior option:selected").val());
   const behavior = $("#select-behavior option:selected").val();
   irisModel.setAgentsBehavior(behavior);
+  const agentsNumber = document.getElementById('how-many-agents');
+  const inputCustom = document.getElementsByClassName('custom-behavior');
+  // const behavior = $("#select-behavior option:selected").val();
+  // here we reset the values of the behaviors to 0
+  for (const el of inputCustom) {
+    el.value = 0;
+  }
+  // hee we update them to all *behavior*
+  switch (behavior) {
+    case 'curious':
+      inputCustom.curious.value = agentsNumber.value;
+      break;
+    case 'perfectionist':
+      inputCustom.perfectionist.value = agentsNumber.value;
+      break;
+    case 'geniesser':
+      inputCustom.geniesser.value = agentsNumber.value;
+      break;
+    case 'capitalist':
+      inputCustom.capitalist.value = agentsNumber.value;
+      break;
+  }
 })
 
-let slider = document.getElementById('min-wage');
-slider.addEventListener('change', event =>{
+let minWage = document.getElementById('min-wage');
+minWage.addEventListener('change', event => {
   // console.log(slider.value);
-  irisModel.setMinWage(slider.value);
+  irisModel.setMinWage(minWage.value);
 })
+
+$('.custom-behavior').change(() => {
+  let sum = 0;
+  const agentsNumber = document.getElementById('how-many-agents');
+  const inputCustom = document.getElementsByClassName('custom-behavior');
+  for (const el of inputCustom) {
+    sum += parseInt(el.value);
+  }
+  console.log(sum);
+  agentsNumber.value = sum;
+});
+
+function restartModel() {
+  const customBehavior = document.getElementsByClassName('custom-behavior');
+  const minWage = document.getElementById('min-wage');
+  const tasksNum = document.getElementById('how-many-task');
+    let obj = {
+    curious: customBehavior.curious.value,
+    perfectionist: customBehavior.perfectionist.value,
+    geniesser: customBehavior.geniesser.value,
+    capitalist: customBehavior.capitalist.value,
+    min_wage: minWage.value,
+    tasks_num: tasksNum.value,
+    players: 0
+  }
+  console.log(obj);
+}
